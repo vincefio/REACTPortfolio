96a4eb861c90ea338ad3d0fde4f0c46fee34263c
vincefio <vincentfiorilli@gmail.com>
2019-01-23 20:35:54 -0500

    i wont stop till i win

--- src/js/script.js
+++ src/js/script.js
@@ -4,31 +4,17 @@ $(document).ready(function () {
 
     $('.sidenav').sidenav();
 
-    /*$('#workLink').on('click', (e) => {
-        e.preventDefault()
-    })*/
-    console.log('type it ' + TypeIt)
-    /*new TypeIt('#catchPhrase', {
-        strings: ["Design...", "Create..."],
-        speed: 50,
-        breakLines: false,
-        waitUntilVisible: true
-    }).go();*/
+
+    $(window).scroll(function(){
+        var positiontop = $(document).scrollTop();
+        console.log(positiontop)
+    })
 
     new TypeIt('#catchPhrase', {
         //strings: ["Design...", "Create...", "Inspire"],
         speed: 40,
         waitUntilVisible: true
       }).go();
-
-   /* $('.content').each(function (i) {
-
-        setTimeout(function () {
-            // console.log(i)
-            $('.content').eq(i).addClass('is-visible')
-        }, 300 * i)
-
-    })*/
 
 
 });

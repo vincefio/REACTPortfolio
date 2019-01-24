1efbe871030c62a61dcd7e615c28a737ab1ff67e
vincefio <vincentfiorilli@gmail.com>
2019-01-23 21:53:14 -0500

    couple hours done

--- src/js/script.js
+++ src/js/script.js
@@ -6,15 +6,43 @@ $(document).ready(function () {
 
 
     $(window).scroll(function(){
-        var positiontop = $(document).scrollTop();
-        console.log(positiontop)
+        var positionTop;
+        positionTop = $(document).scrollTop();
+        console.log(positionTop)
+
+        /*switch(positionTop){
+            case 175:
+            //alert('hit')
+            $('#profilePic').addClass('fadeIn')
+                break;
+              
+            default:
+                console.log(positionTop)
+
+        }*/
+
+        if(positionTop > 155){
+            //$('#aboutMeHeader').addClass('animated rollIn')
+            $('#profilePic').addClass('fadeIn')
+        }
+        if(positionTop > 570){
+            $('.firstTwo').addClass('animated rollIn')
+        }
+        /*if(positionTop > 685){
+            $('.firstTwoWrap').addClass('animated fadeInRightBig')
+        }*/
+        if(positionTop > 800){
+            $('.secondTwo').addClass('animated rollIn')
+        }
+        /*if(positionTop > 917){
+            $('.secondßTwoWrap').addClass('animated fadeInRightBig')
+        }*/ 
     })
 
     new TypeIt('#catchPhrase', {
         //strings: ["Design...", "Create...", "Inspire"],
-        speed: 40,
+        speed:150,
         waitUntilVisible: true
       }).go();
-
 
 });

e1f76c885ab4a6699ece4e06aae148acc3feaa70
vincefio <vincentfiorilli@gmail.com>
2019-01-29 21:05:23 -0500

    got after it today

--- src/js/script.js
+++ src/js/script.js
@@ -5,37 +5,39 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 
 
-    $(window).scroll(function(){
-        var positionTop;
-        positionTop = $(document).scrollTop();
-        console.log(positionTop)
-        //console.log($(window).width())
+        $(window).scroll(function(){
+            var positionTop;
+            positionTop = $(document).scrollTop();
+            console.log(positionTop)
+            //console.log($(window).width())
+    
+                if(positionTop > 155){
+                    //$('#aboutMeHeader').addClass('animated rollIn')
+                    $('#profilePic').addClass('fadeIn')
+                }
+                if(positionTop > 570){
+                    $('.firstTwo').addClass('animated rollIn')
+                }
+                if(positionTop > 800){
+                    $('.secondTwo').addClass('animated rollIn')
+                }
+                if(positionTop > 1230){
+                    $('#portfolioProject1').addClass('animated fadeIn')
+                }
+                if(positionTop > 1530){
+                    $('#portfolioProject2').addClass('animated fadeIn')
+                }
+                if(positionTop > 1830){
+                    $('#portfolioProject3').addClass('animated fadeIn')
+                }
+                if(positionTop > 2130){
+                    $('#portfolioProject4').addClass('animated fadeIn')
+                }
+
+    
+        })
 
 
-       if(positionTop > 155){
-            //$('#aboutMeHeader').addClass('animated rollIn')
-            $('#profilePic').addClass('fadeIn')
-        }
-        if(positionTop > 570){
-            $('.firstTwo').addClass('animated rollIn')
-        }
-        if(positionTop > 800){
-            $('.secondTwo').addClass('animated rollIn')
-        }
-        if(positionTop > 1230){
-            $('#portfolioProject1').addClass('animated fadeIn')
-        }
-        if(positionTop > 1530){
-            $('#portfolioProject2').addClass('animated fadeIn')
-        }
-        if(positionTop > 1830){
-            $('#portfolioProject3').addClass('animated fadeIn')
-        }
-        if(positionTop > 2130){
-            $('#portfolioProject4').addClass('animated fadeIn')
-        }
-
-    })
 
     /*HOVER EVENTS FOR PORTFOLIO*/
     $('.portfolioSingleProjectWrap').hover(
